b695530d6a629a383a4fc3625bef9ddde75dcc3b
seunggun-fundbloc <seunggun@fundbloc.io>
2021-06-16 15:08:06 +0900

    add headers to request for news url to avoid Exception

--- navernewscrawler/naver_crawler.py
+++ navernewscrawler/naver_crawler.py
@@ -20,7 +20,8 @@ def get_news(n_url):
     list
     """
     news_detail = []
-    breq = requests.get(n_url)
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    breq = requests.get(n_url, headers=header)
     bsoup = BeautifulSoup(breq.content, 'html.parser')
     title = bsoup.select('h3#articleTitle')[0].text
     news_detail.append(title)
